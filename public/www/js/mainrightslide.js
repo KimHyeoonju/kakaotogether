5d704aca2a34a8a98eda16c17f7adc98ecf6561c
shparknr11 <shparknr11@naver.com>
2024-05-21 15:33:30 +0900

    docs : main-right swiper 적용완료(reviewslide), header pc menu 작업, 전체 반응형 진행 중.

--- public/www/js/mainrightslide.js
+++ public/www/js/mainrightslide.js
@@ -1,1 +1,37 @@
-window.addEventListener("load", function () {});
+window.addEventListener("load", function () {
+  //console.log("가자");
+  //할일
+  const dataUrl = "./apis/reviewslide.json";
+  fetch(dataUrl)
+    .then((response) => {
+      const result = response.json();
+      return result;
+    })
+    .then((result) => {
+      let tagS = "";
+      for (let i = 0; i < result.length; i++) {
+        const obj = result[i];
+        const temp = `<div class="swiper-slide">
+              <a href="${obj.url}">
+              <strong>${obj.title}</strong>
+                <p>${obj.subtext}</p>
+              </a>
+            </div>`;
+        tagS += temp;
+      }
+      //   console.log(tagS);
+      const whereTag = document.querySelector(".reviewslide .swiper-wrapper");
+      whereTag.innerHTML = tagS;
+      // 슬라이드 작동
+      const bannerSlide = new Swiper(".reviewslide", {
+        loop: true, // 이미지 무한반복
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
